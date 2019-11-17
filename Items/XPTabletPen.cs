73ed760c7ba38af86e5a58ac9d253f5e598d0795
garhu <garhu@garhu.com>
2019-11-16 18:49:06 -0700

    added garhu items

---
+++ Items/XPTabletPen.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace FirstMod.Items
+{
+    public class XPTabletPen : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tablet Pen Star G640");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 30;
+            item.ranged = true;
+            item.width = 10;
+            item.height = 10;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.knockBack = 1f;
+            item.rare = 4;
+
+            item.shoot = mod.ProjectileType("XPTabletPenPro");
+
+            // velocity of the projectile
+            item.shootSpeed = 4.5f;
+
+            item.ammo = AmmoID.Arrow;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            recipe.AddIngredient(ItemID.DirtBlock);
+
+            // recipe.AddTile(TileID.WorkBenches);
+
+            recipe.SetResult(this, 3);
+
+            recipe.AddRecipe();
+        }
+    }
+    
+}
---
+++ Items/Yoyo.cs
@@ -0,0 +1,46 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace FirstMod.Items
+{
+	public class Yoyo : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+            DisplayName.SetDefault("The Zenith"); // DisplayName.SetDefault("FirstSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Blue Zenith 727 lol.");
+		}
+
+		public override void SetDefaults() 
+		{
+            item.useStyle = 5;
+            item.width = 24;
+            item.height = 24;
+            item.useAnimation = 25;
+            item.useTime = 25;
+            item.shootSpeed = 16f;
+            item.knockBack = 1f;
+            item.damage = 72;
+            item.rare = 12;
+
+            item.melee = true;
+            item.channel = true;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+
+            item.UseSound = SoundID.Item1;
+            item.value = Item.sellPrice(silver: 1);
+            item.shoot = mod.ProjectileType("YoyoPro");
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
